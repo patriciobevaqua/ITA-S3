b796281f8d49c261e936c382aa6c040e5298e5af
patriciobevaqua <patriciobq@gmail.com>
2022-04-17 02:49:08 +0200

    suboEjercicio6

--- js/checkout.js
+++ js/checkout.js
@@ -8,7 +8,87 @@ var errorPassword = document.getElementById("errorPassword");
 var errorName = document.getElementById('errorName');  
 var errorPhone = document.getElementById('errorPhone');  
 
+//regular expresions
+var soloChar = /\d+/g;
+var validMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+var alphanum = /^[0-9a-zA-Z]+$/;
+var soloNum = /^[0-9]*$/;
+
 // Exercise 6
 function validate() {
     // Validate fields entered by the user: name, phone, password, and email
+    event.preventDefault();
+
+    var noErrors = true;
+
+    var nameForm = document.getElementById('fName').value;
+    var lastNameForm = document.getElementById('fLastN').value;
+    var emailForm = document.getElementById('fEmail').value;
+    var adressForm = document.getElementById('fAddress').value;
+    var passForm = document.getElementById('fPassword').value;
+    var phoneForm = document.getElementById('fPhone').value;
+
+    //validate name 
+    var nameForm_validate = nameForm.match(soloChar);
+    var groupName = document.getElementById('groupName');
+    if ((nameForm_validate != null) || (nameForm.length <= 2)) {
+        noErrors = false;
+        groupName.classList.add('errorForm');
+    } else {
+        groupName.classList.remove('errorForm');
+    }
+
+    //validate last Name
+    var lastNameForm_validate = lastNameForm.match(soloChar);
+    var groupLastName = document.getElementById('groupLastName');
+    if ((lastNameForm_validate != null) || (lastNameForm.length <= 2)) {
+        noErrors = false;
+        groupLastName.classList.add('errorForm');
+    } else {
+        groupLastName.classList.remove('errorForm');
+    }
+
+    //validate email
+    var mail_validate = emailForm.match(validMail);
+    var groupMail = document.getElementById('groupMail');
+    if ((mail_validate == null) || (emailForm.length < 1)) {
+        noErrors = false;
+        groupMail.classList.add('errorForm');
+    } else {
+        groupMail.classList.remove('errorForm');
+    }
+
+    //validate adress
+    var groupAdress = document.getElementById('groupAdress');
+    if (adressForm.length <= 2) {
+        noErrors = false;
+        groupAdress.classList.add('errorForm');
+    } else {
+        groupAdress.classList.remove('errorForm');
+    }
+
+    //validate password
+    var passwd_validate = passForm.match(alphanum);
+    var groupPasswd = document.getElementById('groupPassword');
+    if ((passwd_validate == null) || (passForm <= 2)) {
+        noErrors = false;
+        groupPasswd.classList.add('errorForm');
+    } else {
+        groupPasswd.classList.remove('errorForm');
+    }
+
+    //validate phone number
+    var phone_validate = phoneForm.match(soloNum);
+    var groupPhone = document.getElementById('groupPhone');
+    if ((phone_validate == null) || (phoneForm.length <= 8)) {
+        noErrors = false;
+        groupPhone.classList.add('errorForm');
+    } else {
+        groupPhone.classList.remove('errorForm');
+    }
+
+    if (noErrors == true) {
+        console.log('no errors');
+    }
+
 }
